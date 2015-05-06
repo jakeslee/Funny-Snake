4b13ef188ca718e30faa4592a96c0887365d0293
Jakes Lee <jakeslee66@gmail.com>
2015-05-07 00:40:48 +0800

    createFood函数实在多余，可以在构造时就实现，故移去

--- src/Game/Snake/Controller/Food.java
+++ src/Game/Snake/Controller/Food.java
@@ -7,16 +7,6 @@ import java.awt.*;
  */
 public interface Food {
     /*
-    * 创建食物
-    *
-    * 注意: 不能与蛇身重叠
-    *
-    * 返回: 新创建的食物
-    * */
-
-    Food createFood();
-
-    /*
     * 是否被吃掉
     *
     * 返回: 吃掉返回true，否则返回false

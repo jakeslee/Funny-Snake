9a95fb62ebf34d1ddc645068196469aa1ee3f5a4
Jakes Lee <jakeslee66@gmail.com>
2015-05-08 02:36:50 +0800

    实现按键响应，完成绘制函数，重新编排函数位置，增加注释

--- src/Game/Snake/Drawer/GameScreen.java
+++ src/Game/Snake/Drawer/GameScreen.java
@@ -28,6 +28,7 @@ class GameScreen extends JPanel {
     private Image imageSnakeBody = null;
     private Image imageSnakeTurn = null;
     private Image imageSnakeTail = null;
+    private Image imageFood = null;
 
     private Snake snake;
     private Food food;
@@ -45,13 +46,24 @@ class GameScreen extends JPanel {
 
     public GameScreen() {
         collideWatcher = new CollideWatcher();
-        snake = new SnakeImpl();
+        snake = Factory.createSnake();
+
+        /*
+        * 蛇死亡事件处理
+        * */
         snake.setDeathListener(new EventProcessAdapter() {
             @Override
             public void eventProcessing() {
 
             }
         });
+
+        /*
+        * 处理刷新事件
+        *
+        * 1. 重绘视口
+        * 2. 触发数据更新事件
+        * */
         snake.setOnRefreshListener(new EventProcessAdapter() {
             @Override
             public void eventProcessing() {
@@ -60,23 +72,30 @@ class GameScreen extends JPanel {
         });
         collideWatcher.add((Collidedable) snake);
 
+        food = Factory.createFood();
 
-        food = new FoodImpl();
+        /*
+        * 食物被吃响应
+        *
+        * 产生新的食物
+        * */
         food.setEatenListener(new EventProcessAdapter() {
             @Override
             public void eventProcessing() {
-                food = new FoodImpl();
+                food = Factory.createFood();
+                food.setEatenListener(this);
                 collideWatcher.set(Food.class.getName(), food);
             }
         });
         collideWatcher.add((Collidedable) food);
 
-
-        setSize(Config.VIEW_SIZE);
-
+        /*
+        * 配置更新事件处理
+        * */
         Config.addUpdateEventListener(new EventProcessAdapter() {
             @Override
             public void updateEvent(Object data) {
+                //BACKGROUND_PATH
                 if (Config.BACKGROUND_PATH != null && Config.BACKGROUND_PATH.equals("")) {
                     try {
                         imageBackground = ImageIO.read(new File(Config.BACKGROUND_PATH));
@@ -87,6 +106,17 @@ class GameScreen extends JPanel {
                     imageBackground = null;
                 }
 
+                //FOOD_IMG
+                if (Config.FOOD_IMG != null) {
+                    try {
+                        imageFood = ImageIO.read(new File(Config.FOOD_IMG));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else
+                    imageFood = null;
+
+                //SNAKE_HEAD_IMG, SNAKE_BODY_IMG, SNAKE_TURN_IMG, SNAKE_TAIL_IMG
                 if (Config.SNAKE_HEAD_IMG != null) {
                     try {
                         imageSnakeHead = ImageIO.read(new File(Config.SNAKE_HEAD_IMG));
@@ -105,26 +135,58 @@ class GameScreen extends JPanel {
             }
         });
 
-
+        /*
+        * 按键事件处理
+        * */
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_UP:
+                        ((Snake) getSnake()).turnUp();
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        ((Snake) getSnake()).turnDown();
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        ((Snake) getSnake()).turnRight();
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        ((Snake) getSnake()).turnLeft();
+                        break;
+                }
+                super.keyPressed(e);
             }
         });
 
+        /*
+        * 窗体配置
+        * */
+        setSize(Config.VIEW_SIZE);
+
     }
 
+    /*
+    * 开始游戏处理函数
+    * */
     public void startGame() {
-        getSnake().start();
+        ((Snake)getSnake()).start();
         starting = true;
     }
 
+    /*
+    * 停止游戏处理函数
+    * */
     public void stopGame() {
-
+        ((Snake)getSnake()).stop();
         starting = false;
     }
 
+    /*
+    * 视口绘制函数
+    *
+    * 参数: graphics  用于绘制的画布
+    * */
     public void paintObject(Graphics2D graphics) {
         //绘制背景
         int bg_width = Config.VIEW_SIZE.width;
@@ -138,34 +200,84 @@ class GameScreen extends JPanel {
         }
 
         //绘制蛇身
-        java.util.List<Rectangle> snake_rect = getSnake().getSnakeNodes();
-        if (Config.SNAKE_HEAD_IMG == null || Config.SNAKE_BODY_IMG  == null ||
-                Config.SNAKE_TAIL_IMG  == null || Config.SNAKE_TURN_IMG  == null) {
-            for (Rectangle rect : snake_rect) {
-                graphics.draw(makeCircle(rect));
+        java.util.List<Rectangle> snake_rect = getSnake().getDrawableArea().rectangles;
+        String method = null;
+        Image IMG = null;
+        for (Rectangle r : snake_rect) {
+            method = getSnake().getDrawableArea().paintMethd.get(r);
+            //绘制对象名称 （SNAKE_HEAD, SNAKE_BODY, SNAKE_TAIL, SNAKE_TURN, FOOD或图片路径）
+            //                          null 标识采用默认值(SNAKE_DEFAULT)+
+            IMG = getImageByMethod(method);
+            if (IMG != null) {
+                graphics.drawImage(IMG, (int)r.getX(), (int)r.getY(), this);
+                continue;
             }
-        }else {
-            for (int i = 0; i < snake_rect.size(); i++) {
-                if (i == 0)
-                    graphics.drawImage(imageSnakeHead, (int)snake_rect.get(i).getX(), (int)snake_rect.get(i).getY(),
-                            (int)snake_rect.get(i).getWidth(), (int)snake_rect.get(i).getHeight(), this);
-                else if (i == snake_rect.size() - 1)
-                    graphics.drawImage(imageSnakeTail, (int)snake_rect.get(i).getX(), (int)snake_rect.get(i).getY(),
-                            (int)snake_rect.get(i).getWidth(), (int)snake_rect.get(i).getHeight(), this);
-                else
-                    graphics.drawImage(imageSnakeBody, (int)snake_rect.get(i).getX(), (int)snake_rect.get(i).getY(),
-                            (int)snake_rect.get(i).getWidth(), (int)snake_rect.get(i).getHeight(), this);
-            }
+            graphics.draw(makeCircle(r));
         }
 
         //绘制食物
-        Rectangle food_rect = getFood().getRect();
-        Color c = graphics.getColor();
-        graphics.setColor(Color.yellow);
-        graphics.draw(makeCircle(food_rect));
-        graphics.setColor(c);
+        Rectangle food_rect = getFood().getDrawableArea().rectangles.get(0);
+        method = getFood().getDrawableArea().paintMethd.get(food_rect);
+        IMG = getImageByMethod(method);
+        if (IMG == null) {
+            Color c = graphics.getColor();
+            graphics.setColor(Color.yellow);
+            graphics.draw(makeCircle(food_rect));
+            graphics.setColor(c);
+        }else {
+            graphics.drawImage(IMG, (int)food_rect.getX(), (int)food_rect.getY(), this);
+        }
+
     }
 
+    /*
+    * 通过绘制方法获取对应图片
+    *
+    * 参数: method    绘制方法
+    *
+    * 返回值: 返回对应的图片，null将使用默认方式绘制
+    * */
+    public Image getImageByMethod(String method) {
+        Image IMG = null;
+        if (method != null) {
+            switch (method) {
+                case "SNAKE_HEAD":
+                    IMG = imageSnakeHead;
+                    break;
+                case "SNAKE_BODY":
+                    IMG = imageSnakeBody;
+                    break;
+                case "SNAKE_TAIL":
+                    IMG = imageSnakeTail;
+                    break;
+                case "SNAKE_TURN":
+                    IMG = imageSnakeTurn;
+                    break;
+                case "FOOD":
+                    IMG = imageFood;
+                    break;
+                default:
+                    try {
+                        IMG = ImageIO.read(new File(method));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+            if (IMG != null) {
+                return IMG;
+            }
+        }
+        return null;
+    }
+
+    /*
+    * 产生一个圆
+    *
+    * 参数: r     圆的参数
+    *
+    * 返回值: 返回圆对象
+    * */
     public Ellipse2D makeCircle(Rectangle r) {
         double x = r.getX() + r.getWidth() / 2;
         double y = r.getY() + r.getHeight() / 2;
@@ -175,6 +287,27 @@ class GameScreen extends JPanel {
         return e;
     }
 
+    /*
+    * 获取蛇的可绘制对象
+    *
+    * 可以转换为其它接口(Snake, Collidedable)
+    * */
+    private Drawable getSnake() {
+        return (Drawable)collideWatcher.get(Snake.class.getName());
+    }
+
+    /*
+    * 获取食物的可绘制对象
+    *
+    * 可以转换为其它接口(Food, Collidedable)
+    * */
+    private Drawable getFood() {
+        return (Drawable)collideWatcher.get(Food.class.getName());
+    }
+
+    /*
+    * 设置更新事件响应
+    * */
     public void setUpdateEventListener(EventProcessListener updateEventListener) {
         this.updateEventListener = updateEventListener;
     }
@@ -195,13 +328,5 @@ class GameScreen extends JPanel {
             }
         }
 
-    }
-
-    private Snake getSnake() {
-        return (Snake)collideWatcher.get(Snake.class.getName());
-    }
-
-    private Food getFood() {
-        return (Food)collideWatcher.get(Food.class.getName());
     }
 }

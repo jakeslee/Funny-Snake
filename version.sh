72556985f86be6190ccc6ac22ea20cfc0532e225
Jakes Lee <jakeslee66@gmail.com>
2015-05-10 23:31:15 +0800

    增加Mac下打包为App的配置，修复工作目录Bug

--- version.sh
+++ version.sh
@@ -12,6 +12,7 @@ MAXVER=$(($VER/100))
 SECVER=$((($VER-100*$MAXVER)/10))
 MINVER=$(($VER-100*$MAXVER-10*$SECVER))
 
+sed -i '' "s/shortversion=\".*\"/shortversion=\"$MAXVER.$SECVER.$MINVER ($SHA1)\"/g" "Ant/build.xml"
 sed -i '' "s/final String Version.*/final String Version = \"$MAXVER.$SECVER.$MINVER ($SHA1)\";/g" "$2"
 
 sleep 1
